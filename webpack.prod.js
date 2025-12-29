d1cac6c4273b076414accadd5add534d37525ea5
LAmena-dev <amena2laremm@gmail.com>
2025-12-29 19:55:28 +0800

    1.1.1 - webpack json edits

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,14 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    publicPath: "/TOP-weather-app/",
+  },
 });
